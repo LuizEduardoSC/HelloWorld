bad3497374b8eda8e354cbe06869b4c961808f2c
LuizEduardoSC <luizeduardoedd1@gmail.com>
2023-12-07 13:49:53 -0300

    Projeto Hello World, Atividade_01

--- src/main/java/com/generation/helloworld/controller/HelloController.java
+++ src/main/java/com/generation/helloworld/controller/HelloController.java
@@ -10,11 +10,24 @@ public class HelloController {
 	
 	@GetMapping
 	public String hello() {
-		return "<b>Hello Turma 68!!!</b>";
+		return "<b>Hello World!</b>";
 	}
 	
-	@GetMapping("/top")
-	public String top() {
-		return "<b>Turma 68 TOP!!!</b>";
+	/*@GetMapping("/bsm")
+	public List<String> bsm() {
+        return Arrays.asList"<b>Persistência</b>", 
+        		"<b>Mentalidade de Crescimento</b>",
+        		"<b>Responsabilidade Pessoal</b>"; 
+	} */
+	
+	@GetMapping("/bsm")
+	public String bsm() {
+		return "<b>Lista de BSM’s da Generation:</b> <br />Persistência<br />Mentalidade de Crescimento<br />Responsabilidade Pessoal<br />"
+	+ "Orientação ao Futuro<br />Trabalho em Equipe<br />Comunicação<br />Proatividade<br />";
+	}
+	
+	@GetMapping("/objetivos")
+	public String objetivos() {
+		return "<b>Objetivos de Aprendizagem da Semana:</b> Aprender e Praticar o Spring, MySQL e o Insomnia";
 	}
 }
